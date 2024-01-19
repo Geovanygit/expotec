8f9c3833a1393a13beb5a7334ee6f87eee12d7ce
Axel Ordoñez <progra@gmail.com>
2024-01-19 11:17:29 -0600

    primer repositorio

---
+++ src/ejemplo2/Ejemplo2.java
@@ -0,0 +1,13 @@
+//Axel Ordoñez
+package ejemplo2;
+
+
+public class Ejemplo2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Axel");
+        System.out.println("Ordoñez");
+    }
+    
+}
